4f7458e83a99b78bc00d4e397b31944ffe932414
kxrthz <karthikeya1x@gmail.com>
2026-08-31 17:36:09 +0530

    fix: update JwtTokenProvider with proper imports and formatting

--- src/main/java/com/fixora/security/JwtTokenProvider.java
+++ src/main/java/com/fixora/security/JwtTokenProvider.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.slf.Logger;
-import org.slf.LoggerFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -68,4 +68,3 @@ public class JwtTokenProvider {
         return false;
     }
 }
-
